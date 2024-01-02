3ba68f8cb39a68f23edb36c6c7b06537c5b6f460
milotriss <nhattienla98@gmail.com>
2024-01-02 10:51:04 +0700

    last

--- src/routers/routers.router.tsx
+++ src/routers/routers.router.tsx
@@ -9,6 +9,7 @@ import Users from "../components/users/users";
 import Products from "../components/products/products";
 import Orders from "../components/orders/orders";
 import Login from "../components/login/login";
+import ForbiddenLayout from "../layouts/forbiddentLayout/forbiddentLayout";
 
 const Routers = (): JSX.Element => {
   return (
@@ -26,7 +27,7 @@ const Routers = (): JSX.Element => {
             <Route path="/order" element={<DefaultLayout son={<Orders />} />} />
           </Route>
         </Route>
-        <Route path="*" element={<h1>Locked</h1>} />
+        <Route path="*" element={<ForbiddenLayout/>} />
       </Routes>
     </>
   );
